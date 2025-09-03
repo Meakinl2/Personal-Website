796a29a23ab818180e2e11bef1b01b5a52409e60
Meakinl2 <meakinl2@coventry.ac.uk>
2025-09-03 14:35:33 +0100

    03/09/25 Pages bases created and linked to flask

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def Main():
     return render_template("homepage.html")
 
+@app.route("/experience")
+def Experience():
+    return render_template("experience.html")
+
+@app.route("/projects")
+def Projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def Contact():
+    return render_template("contact.html")
+
 @app.route("/playground")
 def Playground():
     return render_template("playground.html")
